57ce55b4b7e73455f1db7ba5effb4db7ebf30b06
dpollozhani <drilon.pollozhani@gmail.com>
2021-09-08 09:54:16 +0200

    Updated ahp_bi_tool.py

--- ahp_bi_tool.py
+++ ahp_bi_tool.py
@@ -1,40 +1,34 @@
-import json
 import ahpy
 import pandas as pd
-from datetime import date, datetime
+from datetime import datetime
+import json
 from pprint import pprint
 
-def load_overview_from_excel(file, sheet):
-    df = pd.read_excel(io=file, sheet_name=sheet)
-    return df
-
-def load_pairwise_from_excel(file, sheet):
-    df = pd.read_excel(io=file, sheet_name=sheet)
-    return df
-
-def split_levels(df, level_col):
-    criteria = df[df[level_col] == 'Criteria']
-    subcriteria = df[df[level_col] == 'Subcriteria']
-    alternatives = df[df[level_col] == 'Alternatives']
-    return criteria, subcriteria, alternatives
-
-def criteria_to_tuple(df):
-    comparisons = {}
-    for _, row in df.iterrows():
-        comparisons[(row['A'], row['B'])] = row['Relative importance']
+def load_comparisons(file_name, sheet_name) -> dict:
+    "Load symmetric matrices of comparisons and return as dict of tuples"
+    df = df = pd.read_excel(io=file_name, sheet_name=sheet_name)
+    cols_to_use = [c for c in df.columns if any([isinstance(c, float), isinstance(c, int)]) or "Partition" in c]
+    df = df[cols_to_use]
+    df = df[df["Partition id"] > 0]
+    comparisons = matrix_to_tuples(df)
     return comparisons
 
-def subcriteria_alternatives_to_tuples(df, all_criteria):
+def matrix_to_tuples(df: pd.DataFrame) -> dict:
+    "Return dict of tuples with pairwise criteria/alternatives and each weight as keys"
     comparisons = {}
-    for c in all_criteria:
-        if c in df['Parent'].values:
-            s_df = df[df['Parent'] == c].iterrows()
-            comparisons[c] = {(row['A'],row['B']):row['Relative importance'] for _, row in s_df}
+    for i in set(df["Partition id"].values):
+        df_part_base = df[df["Partition id"]==i]
+        partition = df_part_base["Partition name"].values[0]
+        first_n_cols = list(df_part_base.columns).index("Partition name")
+        new_index = df_part_base.iloc[0,:first_n_cols].values
+        df_part = df_part_base.iloc[1:,:first_n_cols]
+        df_part.index, df_part.columns = new_index, new_index
+        comparisons_base = df_part.unstack().reset_index()
+        comparisons[partition]  = {(row.loc['level_1'], row.loc['level_0']): row.loc[0] for _,row in comparisons_base.iterrows() if row.loc[0] > 0 and row.loc['level_1'] != row.loc['level_0']}
     return comparisons
 
-
-def calculate_weights(level, comparison_name):
-    weights = ahpy.Compare(name=comparison_name, comparisons=level)
+def calculate_weights(comparisons, comparison_name):
+    weights = ahpy.Compare(name=comparison_name, comparisons=comparisons)
     return weights
 
 def save_report(level, to_path):
@@ -42,32 +36,24 @@ def save_report(level, to_path):
     clean_report = {k:v for k,v in report.items() if k in ['name', 'weights', 'consistency_ratio']}
     if '.json' not in to_path:
         to_path += '.json'
-    #pprint(clean_report)
     with open(to_path, 'w') as file:
         json.dump(clean_report, file)
 
-
 def main():
     file_path = r'G:\Business Intelligence\BI strategy\AHP.xlsx'
-    
-    overview = load_overview_from_excel(file=file_path, sheet='Overview')
-    all_criteria = overview[(overview['Criteria'].str.len()>0) & (overview['Criteria']!='None')]['Criteria'].values
-    all_subcriteria = overview[overview['Subcriteria'].str.len()>0]['Subcriteria'].values
+    overview = df = pd.read_excel(io=file_path, sheet_name='Overview')
+
     all_parents = overview[overview['Subcriteria parent'].str.len()>0][['Subcriteria', 'Subcriteria parent']].set_index('Subcriteria')
     
-    pairwise = load_pairwise_from_excel(file=file_path, sheet='Comparison')
-    criteria_base, subcriteria_base, alternatives_base = split_levels(pairwise, 'Hierarchy level')
-    
-    criteria_comparisons = criteria_to_tuple(criteria_base)
-    sub_comparisons = subcriteria_alternatives_to_tuples(subcriteria_base, all_criteria)
-    alt_comparison_parents = list(all_subcriteria) + list(all_criteria)
-    alt_comparisons = subcriteria_alternatives_to_tuples(alternatives_base, alt_comparison_parents)
+    criteria_comparisons = load_comparisons(file_path, sheet_name='Criteria comparisons') 
+    subcriteria_comparisons = load_comparisons(file_path, sheet_name='Subcriteria comparisons') 
+    alternatives_comparisons = load_comparisons(file_path, sheet_name='Alternative comparisons') 
     
     ### Calculating the weights and connecting hierarchy ###
-    criteria = calculate_weights(criteria_comparisons, 'Criteria') #Top criteria
+    criteria = calculate_weights(criteria_comparisons['Criteria'], 'Criteria') #Top criteria
     
     subcriteria = [] #Subcriteria
-    for k,v in sub_comparisons.items():
+    for k,v in subcriteria_comparisons.items():
         subcriteria.append(calculate_weights(v, k))
     
     #Alternatives - connecting to subcriteria and - where applicable - top criteria
@@ -75,8 +61,8 @@ def main():
     parent_model_names = [s.name for s in subcriteria] + [criteria.name]
     alternatives_tmp, alternatives = [], []
     last_parent_name = all_parents.iloc[0,0]
-    max_iter = len(alt_comparisons.keys())
-    for x, (k,v) in enumerate(alt_comparisons.items()):
+    max_iter = len(alternatives_comparisons.keys())
+    for x, (k,v) in enumerate(alternatives_comparisons.items()):
         current_parent_name = all_parents.at[k, 'Subcriteria parent']
         i = parent_model_names.index(current_parent_name)
         j = parent_model_names.index(last_parent_name)
@@ -95,11 +81,9 @@ def main():
             else:
                 criteria_children = subcriteria+alternatives_tmp
                 criteria.add_children(criteria_children)
-        #print(k, ";", current_parent_name)
 
         last_parent_name = current_parent_name
 
-    #criteria.report(show=True)
     file_id = int(datetime.today().timestamp())
     save_report(criteria, to_path=f'G:\Business Intelligence\BI strategy\AHP reports\Top_level_report_{file_id}')
     for s in subcriteria:
@@ -109,3 +93,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
